419bf98602a805420481f5951c82548e94788886
ashishsr71 <ashishsf2002@gmail.com>
2024-07-06 00:51:26 +0530

    search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Flex,Box,  Input, InputGroup, InputLeftElement, InputRightElement } from '@chakra-ui/react';
 import { SearchIcon } from '@chakra-ui/icons';
 import SearchModal from './SearchModal';
-
+import axios from 'axios';
 
 // component starts here
 function Search() {
     const [value,setValue]=useState("");
+    const [result,setResult]=useState(null);
+    const [error,setError]=useState(null);
+    // this useeffect will debounce
+    useEffect(()=>{
+      if(value.length===0)return;
+     const getData=setTimeout(() => {
+      console.log("hii form fimeout")
+      axios.get(`${import.meta.env.VITE_API}/search?username=${value}`).then(res=>{
+        setResult(res.data);
+      }).catch(err=>{setError(err)});
+     }, 1000);
+     return ()=>clearTimeout(getData);
+    },[value]);
 
   return (
     <>
