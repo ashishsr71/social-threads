f2d96d228bce1e985d39ab0bd6883994273cc47a
Ashish Malik <141556047+ashishsr71@users.noreply.github.com>
2024-07-17 12:00:20 +0530

    Api

--- src/components/comment.jsx
+++ src/components/comment.jsx
@@ -9,7 +9,7 @@ const [comments,setComments]=useState([]);
 const token=useSelector(state=>state.auth.token);
 const comState=useSelector(state=>state.comment);
  useEffect(()=>{
- axios.get(`http:/${import.meta.env.VITE_API}/user/getcomments/${post._id}`,{headers:{token}}).then(res=>{
+ axios.get(`${import.meta.env.VITE_API}/user/getcomments/${post._id}`,{headers:{token}}).then(res=>{
   console.log(res.data)
   setComments(res.data)});
  },[comState])
