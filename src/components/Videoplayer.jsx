d2061ba18ded5b995bd92deecbf24a73ed43a12b
ashishsr71 <ashishsf2002@gmail.com>
2024-08-21 02:15:39 +0530

    vvvv

--- src/components/Videoplayer.jsx
+++ src/components/Videoplayer.jsx
@@ -6,7 +6,7 @@ function Videoplayer({stream,callAccepted,callEnded,userVideo,myVideo}) {
   return (
     <div>
        {stream&&  <video ref={myVideo} autoPlay muted style={{  width: "250px"}} />}
-       {callAccepted&&!callEnded&&<video ref={userVideo} autoPlay style={{ width: "250px" }} />}  </div>
+      <video ref={userVideo} autoPlay style={{ width: "250px" }} />  </div>
   )
 }
 
